319c18c41aee12d70845e3876f88dc38b36b6a92
Shahzod2727 <shahzodjava@gmail.com>
2022-03-16 22:56:39 +0500

    part 277

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
-    const params = useParams();
+  const params = useParams();
   return (
     <Fragment>
       <h1>Quote Detail Page</h1>
       <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };
